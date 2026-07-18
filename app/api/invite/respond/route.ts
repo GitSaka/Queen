a74b3226b590a51be7e934a16b19b1b0eb34156f
GitSaka <sakaayedidjou@gmail.com>
2026-07-18 15:37:52 +0200

    Refactor: Déplacement de la route respond dans le dossier api

--- app/api/invite/respond/route.ts
+++ app/api/invite/respond/route.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from '@prisma/client';
 import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
+export const dynamic = 'force-dynamic';
+
 
 export async function POST(request: Request) {
   try {
